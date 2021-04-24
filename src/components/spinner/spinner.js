2279ba30b2db56e1af7de2dee80f369eef99150d
Alex Turaev <turaev@gogpb.ru>
2021-04-24 03:43:11 +0300

    commit verstka OK, spinner OK

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,8 +3,25 @@ import React from 'react';
 import './spinner.scss';
 
 const Spinner = () =>{
+    
     return(
-        <h2 className='spinner'>Loading...</h2>
+        <div className="spinner loadingio-spinner-spinner-epntc31dvh5">
+            <p className='caption-loading'>Loading...</p>
+            <div className="ldio-3q09bi0e1w8">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     );
 }
 
